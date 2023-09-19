bfa9526dbc1e28682253571ac5436b8541ca0348
Kosyan2917 <kosyankov01@gmail.com>
2023-09-19 13:36:32 +0300

    initial commit

---
+++ src/components/board-field.vue
@@ -0,0 +1,17 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "board-field",
+  props: ['id', 'points', ]
+
+}
+
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/board.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "game-board"
+}
+</script>
+
+<style scoped>
+
+</style>
